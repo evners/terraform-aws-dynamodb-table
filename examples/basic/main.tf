f40c859ac93dbe9bb79aa17185c111c50b3e7b00
Hugo Llanos <hugo.llanos@evners.com>
2024-04-24 23:55:43 +0200

    refactor: apply terraform fmt

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,11 +1,11 @@
 # Define a Terraform module for managing a DynamoDB table
 module "dynamodb" {
-  
-  source     = "../.."
-  
+
+  source = "../.."
+
   hash_key   = "LockID"
   table_name = "my-table"
-  
+
   attributes = [{
     type = "S"
     name = "LockID"
